d729a93251150a889b7d3d69739ca31c1c24f637
alexanderaarseth <alexander.aarseth1@gmail.com>
2023-11-19 16:32:48 +0100

    Integrate vipps-payment-service into FishStoreCluster

---
+++ price/src/main/java/com/fishtore/price/PriceApplication.java
@@ -0,0 +1,16 @@
+package com.fishtore.price;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@SpringBootApplication
+@EnableAsync
+public class PriceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PriceApplication.class, args);
+	}
+
+}
+//L480cR68ysW9Sm0q
---
+++ price/src/test/java/com/fishtore/price/staticinventory/StaticinventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fishtore.price.staticinventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StaticinventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
